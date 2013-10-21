de573f6bd1739eb7bb695609e1ba4621348caa80
andre_stylianos <andre.stylianos@gmail.com>
2013-10-20 22:00:33 -0200

    youtube download using dmenu

---
+++ ytmp3.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+#
+#
+
+youtube_icon=$HOME/scripts/icons/youtube.png
+cd $HOME/music/
+notify-send "Youtube: " "Beginning to download mp3..." \
+    -i $youtube_icon
+(youtube-dl -t --extract-audio --audio-format \
+    mp3 "$1" | tail -n 1 | cut -d " " -f 3) > /tmp/songname.txt
+FILENAME=`cat /tmp/songname.txt`
+notify-send "Youtube: " "Downloaded file $FILENAME" \
+    -i $youtube_icon
